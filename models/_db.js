7275d60cb10a76eafb4d4c2c9e1c0429cd155f6f
Jodie Ly <jodie.ly@gmail.com>
2017-03-21 15:41:53 -0400

    working on jquery options

--- models/_db.js
+++ models/_db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('postgres://localhost:5432/tripplanner', {
+const db = new Sequelize('postgres://localhost:5432/tripplannerlive', {
 	logging: false
 });
 
